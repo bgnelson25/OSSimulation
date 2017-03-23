2c858c5d034fbf017b51e635490ef3d5ed7af3a5
carlyfristoe <clfristoe@gmail.com>
2017-03-23 13:20:41 -0500

    test change

---
+++ OSSim/OSSim/Main.cpp
@@ -0,0 +1,33 @@
+//************************************  PROGRAM IDENTIFICATION  ***************************************
+//*                                                                                                   *
+//*		PROGRAM FILE NAME:	Main.cpp			ASSIGNMENT #:	2			Grade: _________          *
+//*                                                                                                   *
+//*		PROGRAM AUTHORS:	_____________________________     __________________________________      *
+//*                                      Carly Fristoe				  Ben Nelson                      *
+//*                                                                                                   *
+//*		COURSE #:	CSC 40600							DUE DATE:	May 5, 2017                       *
+//*                                                                                                   *
+//*****************************************************************************************************
+//************************************  PROGRAM DESCRIPTION  ******************************************
+//*		PROCESS: simulate the operations of an Operating System as it processes a stream of incoming  *
+//*		processes. The purpose of the simulation is to compare the results of at least two different  *
+//*		scheduling algorithms and their effect on system performance. This simulation will employ only*
+//*		one processor (CPU), one I/O device, and one each Long Term Queue, Short Term Queue and I/O   *
+//*		Queue. All statistical calculations are to be made using a clock tick as the unit of measure  *
+//*		for the simulation. All processed will be non-preemptive.									  *
+//*****************************************************************************************************
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <ostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+using namespace std;
+//*********************************FUNCTION Main*******************************************************
+int main()
+{
+	//This is a test comment
+	return 0;
+}
+//******************************END OF FUNCTION Main***************************************************
